da5971bd1bcda30db4f71cabd312554bd33b7597
cameelo <cjsatut@gmail.com>
2021-09-22 16:35:35 -0300

    The scene now is loaded from xml for both simulation and auralization modes.

--- AudioRendering/Camera.h
+++ AudioRendering/Camera.h
@@ -22,7 +22,7 @@ class Camera {
 		glm::mat4 projectionMatrix;
 
 		Camera() {};
-		Camera(float width, float height, float fov, SDL_Window* win);
+		Camera(glm::vec3 pos, float width, float height, float fov, SDL_Window* win);
 		Camera(glm::vec3 pos, glm::vec3 ref, glm::vec3 up, float fov, float sensitivity, float speed, SDL_Window* win, float width, float height);
 		void update();
 		void moveCam();
